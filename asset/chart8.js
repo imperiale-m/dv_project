69ece546237b2b03bfb5ef9d60b08ae8469d60da
AG-98 <gjergjiandrea7@gmail.com>
2023-02-20 22:21:02 +0100

    swap axis and add options

--- asset/chart8.js
+++ asset/chart8.js
@@ -1,11 +1,11 @@
 // Task 5
 // Data
-d3.csv('data/preprocessedData.csv', d3.autoType)
+d3.csv('data/eurostat_data_2.csv', d3.autoType)
   .then((data) => {
     // console.log(data);
 
     // List of groups (here I have one group per column)
-    const allGroup = ['CO2', 'Nitrous oxide'];
+    const allGroup = ['CO2', 'Nitrous oxide', 'PM2.5', 'PM10'];
 
     // add the options to the button
     d3.select('#selectButtonBubble')
@@ -15,6 +15,23 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
       .append('option')
       .text((d) => d) // text showed in the menu
       .attr('value', (d) => d); // corresponding value returned by the button
+
+    const convertName = (label) => {
+      switch (label) {
+        case 'co2_metric_tons_per_capita':
+          return 'CO2';
+        case 'nitrous_oxide_metric_tons':
+          return 'Nitrous oxide';
+        case 'pm2_5':
+          return 'PM2.5';
+        case 'pm10':
+          return 'PM10';
+        case 'life_expectancy_total':
+          return 'Life expectancy';
+        default:
+          return 'Error';
+      }
+    };
 
     function drawChart(filtData, x, y, z) {
       const margin = {
@@ -26,17 +43,20 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
       const width = 600;
       const height = 400;
 
-      const xDomain = [0, d3.max(filtData, (d) => d[x])];
-      const xRange = [0, width];
-
       const yDomain = [0, d3.max(filtData, (d) => d[y])];
       const yRange = [height, 0];
 
       const zDomain = d3.extent(filtData, (d) => d[z]);
       const zRange = [1, 16];
 
+      // Domain excluding zeroes
+      const dom = d3.extent(
+        filtData.filter((d) => d[x] !== 0),
+        (d) => d[x],
+      );
+
       // Construct scales and axes.
-      const xScale = d3.scaleLinear(xDomain, xRange).nice();
+      const xScale = d3.scaleLinear().domain(dom).nice().range([0, width]);
       const yScale = d3.scaleLinear(yDomain, yRange).nice();
       // Add a scale for bubble size
       const zScale = d3.scaleLinear(zDomain, zRange);
@@ -44,7 +64,7 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
       const xAxis = d3.axisBottom(xScale);
       const yAxis = d3.axisLeft(yScale);
 
-      const names = filtData.map((d) => d.Location);
+      const names = filtData.map((d) => d.country);
       const species = [...new Set(names)];
       // Add a scale for bubble color
       const color = d3.scaleOrdinal().domain(species).range(d3.schemeCategory10);
@@ -83,10 +103,12 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
       };
 
       const mousemove = function (event, d) {
+        const xLabel = convertName(x);
+        const yLabel = convertName(y);
         tooltip
           .html(
-            `Country = <b>${d.Location}</b><br>${x} = <b>${d[x]} years</b>
-                    <br>${y} = <b>${d[y]}</b>
+            `Country = <b>${d.country}</b><br>${xLabel} = <b>${d[x]}</b>
+                    <br>${yLabel} = <b>${d[y]}</b>
                     <br>${z} = <b>${d[z]}</b>`,
           )
           .style('top', `${event.pageY}px`)
@@ -96,12 +118,12 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
       svg
         .append('g')
         .selectAll('dot')
-        .data(filtData)
+        .data(filtData.filter((d) => d[x] !== 0))
         .join('circle')
         .attr('cx', (d) => xScale(d[x]))
         .attr('cy', (d) => yScale(d[y]))
         .attr('r', (d) => zScale(d[z]))
-        .style('fill', (d) => color(d.Location))
+        .style('fill', (d) => color(d.country))
         .style('opacity', '0.7')
         .attr('stroke', 'black')
         .style('stroke-width', '1px')
@@ -117,14 +139,14 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
         .attr('text-anchor', 'middle') // this makes it easy to centre the text as the transform is applied to the anchor
         .attr('transform', `translate(${width / 2}, ${height + margin.b / 2})`)
         .attr('class', 'axis-name')
-        .text(x);
+        .text(convertName(x));
       // y-axis name
       svg
         .append('text')
         .attr('text-anchor', 'middle') // this makes it easy to centre the text as the transform is applied to the anchor
         .attr('transform', `translate(${margin.l / 2 - padding},${height / 2})rotate(-90)`) // text is drawn off the screen top left, move down and out and rotate
         .attr('class', 'axis-name')
-        .text(y);
+        .text('Life expectancy (years)');
       // add title
       svg
         .append('text')
@@ -138,20 +160,13 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
     function update(selectedGroup, period) {
       d3.selectAll('#chart8 > svg').remove();
 
-      const filtData = data.filter(
-        (d) => !isNaN(d['Life expectancy at birth, total (years)'])
-          && d['Life expectancy at birth, total (years)'] !== '..'
-          && d['Current health expenditure (% of GDP)'] !== '..'
-          && d[selectedGroup] !== '..'
-          && d.Period === period,
-      );
+      // group data by 'time_period'
+      const dataByYear = d3.group(data, (d) => d.time_period);
+      // console.log(dataByYear.get(2009));
 
-      drawChart(
-        filtData,
-        'Life expectancy at birth, total (years)',
-        'Current health expenditure (% of GDP)',
-        selectedGroup,
-      );
+      const filtData = dataByYear.get(period);
+
+      drawChart(filtData, selectedGroup, 'life_expectancy_total', 'gdp');
     }
 
     // When the button is changed, run the updateChart function
@@ -161,16 +176,16 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
 
       switch (selectedOption) {
         case 'CO2':
-          selectedOption = 'CO2 emissions (metric tons per capita)';
-          break;
-        case 'SF6':
-          selectedOption = 'SF6 gas emissions (thousand metric tons of CO2 equivalent)';
+          selectedOption = 'co2_metric_tons_per_capita';
           break;
         case 'Nitrous oxide':
-          selectedOption = 'Nitrous oxide emissions (thousand metric tons of CO2 equivalent)';
+          selectedOption = 'nitrous_oxide_metric_tons';
           break;
-        case 'HFC':
-          selectedOption = 'HFC gas emissions (thousand metric tons of CO2 equivalent)';
+        case 'PM2.5':
+          selectedOption = 'pm2_5';
+          break;
+        case 'PM10':
+          selectedOption = 'pm10';
           break;
         default:
           break;
@@ -179,7 +194,7 @@ d3.csv('data/preprocessedData.csv', d3.autoType)
       // run the updateChart function with this selected option
       update(selectedOption, 2015);
     });
-    update('CO2 emissions (metric tons per capita)', 2015);
+    update('co2_metric_tons_per_capita', 2015);
   })
   .catch((e) => {
     console.log(e);
