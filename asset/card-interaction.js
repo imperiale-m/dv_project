25745b959adc6817cb77103fd02578d76e8c834c
massi <57863485+imperiale-x@users.noreply.github.com>
2023-02-24 17:35:43 +0100

    update index.html

--- asset/card-interaction.js
+++ asset/card-interaction.js
@@ -1,12 +1,12 @@
 function expandCard(card, open, close) {
-  d3.selectAll('#blur').classed('hidden', false);
+  d3.selectAll('#modal').classed('hidden', false);
   d3.select(card).classed('active', true);
   d3.select(open).classed('hidden', true);
   d3.select(close).classed('hidden', false);
 }
 
 function closeCard(card, open, close) {
-  d3.selectAll('#blur').classed('hidden', true);
+  d3.selectAll('#modal').classed('hidden', true);
   d3.select(card).classed('active', false);
   d3.select(open).classed('hidden', false);
   d3.select(close).classed('hidden', true);
