ff1dd0e2eea2c1b56ff754a936c8a87176d5ca00
massi <57863485+imperiale-x@users.noreply.github.com>
2023-02-26 18:13:31 +0100

    add year selector for chart6

--- asset/card-interaction.js
+++ asset/card-interaction.js
@@ -1,15 +1,33 @@
 function expandCard(card, open, close) {
-  d3.selectAll('#modal').classed('hidden', false);
+  d3.select('#modal').classed('hidden', false);
   d3.select(card).classed('active', true);
   d3.select(open).classed('hidden', true);
   d3.select(close).classed('hidden', false);
+  document.body.style.overflow = 'hidden';
   document.documentElement.requestFullscreen();
 }
 
 function closeCard(card, open, close) {
-  d3.selectAll('#modal').classed('hidden', true);
+  d3.select('#modal').classed('hidden', true);
   d3.select(card).classed('active', false);
   d3.select(open).classed('hidden', false);
   d3.select(close).classed('hidden', true);
+  document.body.style.overflow = 'auto';
   document.exitFullscreen();
+}
+
+function inc(element) {
+  const el = document.querySelector(`#${element}`);
+  if (parseInt(el.value, 10) < 2021) {
+    el.value = parseInt(el.value, 10) + 1;
+    updateChart6(parseInt(el.value, 10));
+  }
+}
+
+function dec(element) {
+  const el = document.querySelector(`#${element}`);
+  if (parseInt(el.value, 10) > 2010) {
+    el.value = parseInt(el.value, 10) - 1;
+    updateChart6(parseInt(el.value, 10));
+  }
 }
