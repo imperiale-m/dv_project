a98757fd7fd55b6c7a12701af2423d12b5d13df2
AG-98 <gjergjiandrea7@gmail.com>
2023-02-26 21:52:49 +0100

    update map functions

--- asset/chart1.js
+++ asset/chart1.js
@@ -41,32 +41,7 @@ Promise.all([
       l: 60,
     };
     const width = 600;
-    const height = 600;
-
-    // group data by 'geo'
-    const dataByYear = d3.group(data[1], (d) => d.time_period);
-
-    const dataBySelectedYear = dataByYear.get(2018) ?? 0;
-    // group data by 'geo'
-    const dataByGeo = d3.group(dataBySelectedYear, (d) => d.geo);
-    // console.log(geoData.features);
-    // console.log(dataByGeo.get('IT')[0].life_expectancy_total);
-
-    // List of subgroups
-    const euCountries = [...dataByGeo.keys()];
-    // console.log(euCountries);
-
-    const filteredEuFeatures = geoData.features.filter((feature) =>
-      euCountries.includes(feature.id),
-    );
-
-    const filteredNotEuFeatures = geoData.features.filter(
-      (feature) => !euCountries.includes(feature.id),
-    );
-
-    const finalMap = [...filteredNotEuFeatures, ...filteredEuFeatures];
-
-    // console.log('Final', finalMap);
+    const height = 500;
 
     // append the svg object to the div with id #a3_task1
     const svg = d3
@@ -77,123 +52,179 @@ Promise.all([
       .append('g')
       .attr('transform', `translate(${margin.l}, ${margin.t})`);
 
-    const projection = d3
-      .geoTransverseMercator()
-      .center([15, 50])
-      .scale(700)
-      .translate([width / 2, height / 2]);
+    // group data by 'year'
+    const dataByYear = d3.group(data[1], (d) => d.time_period);
 
-    const pathGenerator = d3.geoPath().projection(projection);
+    function updateChart1(year) {
+      svg.selectAll('*').remove();
 
-    const tooltip = d3.select('#chart1').append('div').attr('class', 'tooltip');
+      const dataBySelectedYear = dataByYear.get(year) ?? 0;
+      // group data by 'geo'
+      const dataByGeo = d3.group(dataBySelectedYear, (d) => d.geo);
+      // console.log(geoData.features);
+      // console.log(dataByGeo.get('IT')[0].life_expectancy_total);
 
-    const mouseover = function () {
-      if (this.getAttribute('fill') !== '#e0dfdf') {
-        tooltip.style('z-index', 1);
-        tooltip.transition().style('opacity', 0.9);
-        d3.select(this).transition().attr('fill', 'gold');
-      }
-    };
+      // List of subgroups
+      const euCountries = [...dataByGeo.keys()];
+      // console.log(euCountries);
 
-    const mouseout = function (event, d) {
-      tooltip.style('z-index', -1);
-      tooltip.transition().style('opacity', 0);
-      if (this.getAttribute('fill') !== '#e0dfdf') {
-        d3.select(this)
-          .transition()
-          .attr('fill', colorScale(dataByGeo.get(d.id)[0].life_expectancy_total));
-      }
-    };
+      const filteredEuFeatures = geoData.features.filter((feature) =>
+        euCountries.includes(feature.id),
+      );
 
-    // const f = d3.format('.2f');
-    const mousemove = function (event, d) {
-      if (this.getAttribute('fill') !== '#e0dfdf') {
-        const countryData = dataByGeo.get(d.id)[0];
-        tooltip
-          .html(
-            `<b>${countryData.country}</b><br>
-           Life expectancy = <b>${countryData.life_expectancy_total} years</b>`,
-          )
-          .style('top', `${event.pageY}px`)
-          .style('left', `${event.pageX + 20}px`);
-      }
-    };
+      const filteredNotEuFeatures = geoData.features.filter(
+        (feature) => !euCountries.includes(feature.id),
+      );
 
-    svg
-      .append('path')
-      .attr('d', pathGenerator({ type: 'Sphere' }))
-      .attr('fill', 'lightcyan');
+      const finalMap = [...filteredNotEuFeatures, ...filteredEuFeatures];
 
-    const g = svg.append('g');
+      // console.log('Final', finalMap);
 
-    const states = g
-      .selectAll('path')
-      .data(finalMap)
-      .join('path')
-      .attr('d', pathGenerator)
-      .attr('fill', (d) => {
-        if (euCountries.includes(d.id)) {
-          d.lifeExp = dataByGeo.get(d.id)[0].life_expectancy_total;
-          return colorScale(d.lifeExp);
+      const projection = d3
+        .geoTransverseMercator()
+        .center([15, 50])
+        .scale(700)
+        .translate([width / 2, height / 2]);
+
+      const pathGenerator = d3.geoPath().projection(projection);
+
+      const tooltip = d3.select('#chart1').append('div').attr('class', 'tooltip');
+
+      const mouseover = function () {
+        if (this.getAttribute('fill') !== '#e0dfdf' && this.getAttribute('fill') !== 'gray') {
+          tooltip.style('z-index', 1);
+          tooltip.transition().style('opacity', 0.9);
+          d3.select(this).transition().attr('fill', 'gold');
         }
-        return '#e0dfdf';
-      })
-      .style('stroke', (d) => {
-        if (euCountries.includes(d.id)) return 'white';
-        return 'none';
-      })
-      .style('stroke-width', '0.5px')
-      .on('mouseover', mouseover)
-      .on('mouseout', mouseout)
-      .on('mousemove', mousemove);
+        if (this.getAttribute('fill') === 'gray') {
+          tooltip.style('z-index', 1);
+          tooltip.transition().style('opacity', 0.9);
+        }
+      };
 
-    function zoomed(event) {
-      const { transform } = event;
-      g.attr('transform', transform);
-      g.attr('stroke-width', 1 / transform.k);
-    }
+      const mouseout = function (event, d) {
+        tooltip.style('z-index', -1);
+        tooltip.transition().style('opacity', 0);
+        if (this.getAttribute('fill') !== '#e0dfdf' && this.getAttribute('fill') !== 'gray') {
+          d3.select(this)
+            .transition()
+            .attr('fill', colorScale(dataByGeo.get(d.id)[0].life_expectancy_total));
+        }
+      };
 
-    // Redraw the map with the updated projection
-    g.selectAll('path').attr('d', pathGenerator);
+      // const f = d3.format('.2f');
+      const mousemove = function (event, d) {
+        if (this.getAttribute('fill') !== '#e0dfdf' && this.getAttribute('fill') !== 'gray') {
+          const countryData = dataByGeo.get(d.id)[0];
+          tooltip
+            .html(
+              `<b>${countryData.country}</b><br>
+           Life expectancy = <b>${countryData.life_expectancy_total} years</b>`,
+            )
+            .style('top', `${event.pageY}px`)
+            .style('left', `${event.pageX + 20}px`);
+        }
+        if (this.getAttribute('fill') === 'gray') {
+          const countryData = dataByGeo.get(d.id)[0];
+          tooltip
+            .html(
+              `<b>${countryData.country}</b><br>
+           No data available fo this year`,
+            )
+            .style('top', `${event.pageY}px`)
+            .style('left', `${event.pageX + 20}px`);
+        }
+      };
 
-    const zoom = d3.zoom().scaleExtent([1, 8]).on('zoom', zoomed);
-
-    svg.call(zoom);
-
-    function reset() {
-      states.transition().style('fill', null);
       svg
-        .transition()
-        .duration(750)
-        .call(
-          zoom.transform,
-          d3.zoomIdentity,
-          d3.zoomTransform(svg.node()).invert([width / 2, height / 2]),
-        );
-    }
+        .append('path')
+        .attr('d', pathGenerator({ type: 'Sphere' }))
+        .attr('fill', 'lightcyan');
 
-    function clicked(event, d) {
-      if (this.getAttribute('fill') !== '#e0dfdf') {
-        const [[x0, y0], [x1, y1]] = pathGenerator.bounds(d);
-        event.stopPropagation();
+      const g = svg.append('g');
+
+      const states = g
+        .selectAll('path')
+        .data(finalMap)
+        .join('path')
+        .attr('d', pathGenerator)
+        .attr('fill', (d) => {
+          if (euCountries.includes(d.id)) {
+            d.lifeExp = dataByGeo.get(d.id)[0].life_expectancy_total;
+            d.gdp = dataByGeo.get(d.id)[0].gdp;
+            return d.lifeExp ? colorScale(d.lifeExp) : 'gray';
+          }
+          return '#e0dfdf';
+        })
+        .style('stroke', (d) => {
+          if (euCountries.includes(d.id)) return 'white';
+          return 'none';
+        })
+        .style('stroke-width', '0.5px')
+        .on('mouseover', mouseover)
+        .on('mouseout', mouseout)
+        .on('mousemove', mousemove);
+
+      function zoomed(event) {
+        const { transform } = event;
+        g.attr('transform', transform);
+        g.attr('stroke-width', 1 / transform.k);
+      }
+
+      // Redraw the map with the updated projection
+      g.selectAll('path').attr('d', pathGenerator);
+
+      const zoom = d3.zoom().scaleExtent([1, 8]).on('zoom', zoomed);
+
+      svg.call(zoom).on('wheel.zoom', null);
+
+      function reset() {
         states.transition().style('fill', null);
-        d3.select(this).transition().style('fill', 'gold');
         svg
           .transition()
           .duration(750)
           .call(
             zoom.transform,
-            d3.zoomIdentity
-              .translate(width / 2, height / 2)
-              .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-              .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-            d3.pointer(event, svg.node()),
+            d3.zoomIdentity,
+            d3.zoomTransform(svg.node()).invert([width / 2, height / 2]),
           );
       }
-    }
 
-    g.selectAll('path').on('click', clicked);
-    svg.on('click', reset);
+      function clicked(event, d) {
+        if (this.getAttribute('fill') !== '#e0dfdf') {
+          const [[x0, y0], [x1, y1]] = pathGenerator.bounds(d);
+          event.stopPropagation();
+          states.transition().style('fill', null);
+          // console.log(d3.select(this).data()[0].properties.NAME_ENGL);
+          if (this.getAttribute('fill') === 'gray') {
+            d3.select('#countryValue').text(d3.select(this).data()[0].properties.NAME_ENGL);
+            d3.select('#lifeExpValue').text('No data');
+            d3.select('#gdpValue').text(d3.select(this).data()[0].gdp);
+          } else {
+            d3.select('#countryValue').text(d3.select(this).data()[0].properties.NAME_ENGL);
+            d3.select('#lifeExpValue').text(`${d3.select(this).data()[0].lifeExp} years`);
+            d3.select('#gdpValue').text(d3.select(this).data()[0].gdp);
+            d3.select(this).transition().style('fill', 'gold');
+          }
+          svg
+            .transition()
+            .duration(750)
+            .call(
+              zoom.transform,
+              d3.zoomIdentity
+                .translate(width / 2, height / 2)
+                .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+                .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+              d3.pointer(event, svg.node()),
+            );
+        }
+      }
+
+      g.selectAll('path').on('click', clicked);
+      svg.on('click', reset);
+    }
+    window.updateChart1 = updateChart1;
+    updateChart1(2010);
   })
   .catch((e) => {
     console.log(e);
